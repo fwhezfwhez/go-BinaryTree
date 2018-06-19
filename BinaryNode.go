205c244f3aa0e652e5c7b42e561fdabce5224d30
fwhez <1728565484@qq.com>
2018-06-19 17:33:01 +0800

    add GetDepth(node),GetMaxDepth(),GetDistance(node1,node2),GetMaxDistance()

--- BinaryNode.go
+++ BinaryNode.go
@@ -12,11 +12,11 @@ type BinaryNode struct {
 	Right  *BinaryNode
 }
 
-type AVL struct{
+type AVL struct {
 	root *BinaryNode
 }
 
-type RedBlack struct{
+type RedBlack struct {
 	root *BinaryNode
 }
 
@@ -69,10 +69,10 @@ func (bt *BinaryNode) FindMin() (*BinaryNode, error) {
 }
 
 //insert a data
-func (bt *BinaryNode) Insert(x interface{}) (error) {
+func (bt *BinaryNode) Insert(x interface{}) (*BinaryNode, error) {
 	rs, err := Compare(bt.Data, x)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	if rs == 1 {
@@ -80,90 +80,189 @@ func (bt *BinaryNode) Insert(x interface{}) (error) {
 			return bt.Left.Insert(x)
 		} else {
 			bt.Left = NewWithParent(x, bt)
+			return bt.Left, nil
 		}
 	} else {
 		if bt.Right != nil {
 			return bt.Right.Insert(x)
 		} else {
 			bt.Right = NewWithParent(x, bt)
+			return bt.Right, nil
 		}
 
 	}
-	return nil
+	return nil, nil
 }
 
 //delete a node which's data is x,if several nodes' data are all x,only delete one of them
-func (bt *BinaryNode) Remove(x interface{}) (*BinaryNode,error) {
-	if bt ==nil {
-		return nil,nil
+func (bt *BinaryNode) Remove(x interface{}) (*BinaryNode, error) {
+	if bt == nil {
+		return nil, nil
 	}
-	rs,er:=Compare(bt.Data,x)
-	if er!=nil{
-		return nil,er
+	rs, er := Compare(bt.Data, x)
+	if er != nil {
+		return nil, er
 	}
-	if rs ==-1 {
-		bt.Right,er= bt.Right.Remove(x)
-		if er!=nil{
-			return nil,er
+	if rs == -1 {
+		bt.Right, er = bt.Right.Remove(x)
+		if er != nil {
+			return nil, er
 		}
-	}else if rs==1 {
-		bt.Left,er = bt.Left.Remove(x)
-		if er!=nil{
-			return nil,er
+	} else if rs == 1 {
+		bt.Left, er = bt.Left.Remove(x)
+		if er != nil {
+			return nil, er
 		}
-	}else if bt.Left!=nil && bt.Right!=nil {
-		tmp,_:= bt.Right.FindMin()
-		bt.Right,_ = bt.Right.Remove(tmp.Data)
-	}else{
-		if bt.Left!=nil{
+	} else if bt.Left != nil && bt.Right != nil {
+		tmp, _ := bt.Right.FindMin()
+		bt.Right, _ = bt.Right.Remove(tmp.Data)
+	} else {
+		if bt.Left != nil {
 			bt = bt.Left
-		}else{
+		} else {
 			bt = bt.Right
 		}
 	}
 
-	return bt,nil
+	return bt, nil
+}
+
+//get a node 's depth in a bt
+func (bt *BinaryNode) GetDepth(node *BinaryNode) (int, error) {
+	rs, er := Compare(node.Data, bt.Data)
+	if er != nil {
+		return -1, er
+	}
+	if rs == -1 {
+		tmpLeft, _ := bt.Left.GetDepth(node)
+		return tmpLeft + 1, nil
+	} else if rs == 0 {
+		return 0, nil
+	} else {
+		tmpRight, _ := bt.Right.GetDepth(node)
+		return tmpRight + 1, nil
+	}
+
 }
 
 //Get the nearest ancestor of two node
-func (bt *BinaryNode)GetAncestor(node1,node2 *BinaryNode) (*BinaryNode,error){
-	rs1,er:=Compare(node1.Data, bt.Data)
-	if er!=nil{
-		return nil,er
+func (bt *BinaryNode) GetAncestor(node1, node2 *BinaryNode) (*BinaryNode, error) {
+	rs1, er := Compare(node1.Data, bt.Data)
+	if er != nil {
+		return nil, er
 	}
-	rs2,er:=Compare(node2.Data, bt.Data)
-	if er!=nil{
-		return nil,er
+	rs2, er := Compare(node2.Data, bt.Data)
+	if er != nil {
+		return nil, er
 	}
-	if rs1==1 && rs2 ==1 {
-		return bt.Right.GetAncestor(node1,node2)
-	}else if rs1==-1 && rs2==-1{
-		return bt.Left.GetAncestor(node1,node2)
-	}else{
-		return bt,nil
+	if rs1 == 1 && rs2 == 1 {
+		return bt.Right.GetAncestor(node1, node2)
+	} else if rs1 == -1 && rs2 == -1 {
+		return bt.Left.GetAncestor(node1, node2)
+	} else {
+		return bt, nil
 	}
+}
+
+//Assume node1 and node2 is contained,get the nearest distance of two node,however,it didn't support compare two nodes which have the same data,New(11) and new(11) is uncomparable
+//if node1 or node2 isn't included,nil pointer error occurred
+func (bt *BinaryNode) GetDistance(node1, node2 *BinaryNode) (int, error) {
+	ancestor, er := bt.GetAncestor(node1, node2)
+	if er != nil {
+		return -1, er
+	}
+	tmp1, er := ancestor.GetDepth(node1)
+	if er != nil {
+		return -1, er
+	}
+	tmp2, er := ancestor.GetDepth(node2)
+	if er != nil {
+		return -1, er
+	}
+	return tmp1 + tmp2, nil
+}
+
+//get max depth of a bt, returns maxDepth,leftMaxDepth,rightMaxDepth,error
+func (bt *BinaryNode) GetMaxDepth() (int,int,int,error) {
+	if bt == nil {
+		return 0,0,0, nil
+	}
+	var leftMax, rightMax int
+	var er error
+	if bt.Left != nil {
+		leftMax,_,_, er = bt.Left.GetMaxDepth()
+		if er != nil {
+			return -1,-1,-1, er
+		}
+		leftMax++
+	}
+	if bt.Right != nil {
+		rightMax,_,_, er = bt.Right.GetMaxDepth()
+		if er != nil {
+			return -1,-1,-1, er
+		}
+		rightMax++
+	}
+	if leftMax > rightMax {
+		return leftMax,leftMax,rightMax, nil
+	}
+	return rightMax,leftMax,rightMax, nil
+
+}
+
+func (bt *BinaryNode) GetMaxDistance() (int, error) {
+	if bt == nil {
+		return 0, nil
+	}
+	var lMax1 = 0
+	var rMax1 = 0
+	var lMax2 = 0
+	var rMax2 = 0
+	var er error
+
+	_,lMax1,rMax1,er=bt.GetMaxDepth()
+	if bt.Left != nil {
+		lMax2, er = bt.Left.GetMaxDistance()
+		if er != nil {
+			return -1, er
+		}
+		if bt.Right!=nil{
+			lMax2++
+		}
+	}
+	if bt.Right != nil {
+
+		rMax2, er = bt.Right.GetMaxDistance()
+		if er != nil {
+			return -1, er
+		}
+		if bt.Left!=nil{
+			rMax2++
+		}
+	}
+	return max((lMax1+rMax1), lMax2, rMax2), nil
 }
 
 //transfer a bt to a redBlack pattern
 //what is a red-black tree,refer to www.baidu.com, www.google.com
-func ToRedBlack()(*BinaryNode,error){
-	return nil,nil
+func ToRedBlack() (*BinaryNode, error) {
+	return nil, nil
 }
 
 //transfer a bt to an avl parttern
 //what is an avl tree,refer to www.baidu.com,www.google.com
-func ToAVL()(*BinaryNode,error){
-	return nil,nil
+func ToAVL() (*BinaryNode, error) {
+	return nil, nil
 }
 
 //rotate left
-func (av *RedBlack) LeftRotate()(*AVL,error){
-	return nil,nil
+func (av *RedBlack) LeftRotate() (*AVL, error) {
+	return nil, nil
 }
 
 //rotate right
-func (av *RedBlack) RightRotate()(*AVL,error){
-	return nil,nil
+func (av *RedBlack) RightRotate() (*AVL, error) {
+	return nil, nil
 }
 
 //compare two interface{} value ,only supports for int,int64,int32,int16,int8,string,float32,float64
@@ -257,3 +356,14 @@ func Compare(v1 interface{}, v2 interface{}) (int, error) {
 	return -1, errors.New("only support compare int,int8,int16,int32,int64,float32,float64,string")
 }
 
+func max(args ... int) int {
+	var maxTmp = args[0]
+	for i := 0; i < len(args)-1; i++ {
+		for j := i + 1; j < len(args); j++ {
+			if maxTmp < args[j] {
+				maxTmp = args[j]
+			}
+		}
+	}
+	return maxTmp
+}
--- BinaryNode_test.go
+++ BinaryNode_test.go
@@ -4,11 +4,12 @@ import "testing"
 var node,node2,node3,node4,node5 *BinaryNode
 func Init(){
 	node=New(7)
-	node.Insert(5)
+//	node2,_ =node.Insert(5)
 	node.Insert(9)
 	node.Insert(11)
 	node.Insert(11)
 	node.Insert(10)
+	node.Insert(8)
 	//node2=New(5)
 	//node3=New(9)
 	//node4=New(11)
@@ -42,7 +43,7 @@ func TestBinaryNode_FindMin(t *testing.T) {
 func TestBinaryNode_Insert(t *testing.T) {
 	Init()
 	node.Insert(1)
-	er:=node.Insert(15)
+	_,er:=node.Insert(15)
 	if er!=nil{
 		t.Fatal(er)
 	}
@@ -65,4 +66,25 @@ func TestGetAncestor(t *testing.T) {
 	n1 := New(8)
 	n2 :=New(11)
 	t.Log(node.GetAncestor(n1,n2))
+}
+func TestBinaryNode_GetDepth(t *testing.T) {
+	Init()
+	t.Log(node.GetDepth(New(8)))
+}
+
+func TestBinaryNode_GetDistance(t *testing.T) {
+	Init()
+	t.Log(node.GetDistance(New(1),New(11)))
+}
+
+func TestBinaryNode_GetMaxDepth(t *testing.T) {
+	Init()
+	t.Log(node.GetMaxDepth())
+}
+func TestBinaryNode_GetMaxDistance(t *testing.T) {
+	Init()
+	t.Log(node.GetMaxDistance())
+}
+func TestMmax(t *testing.T){
+	t.Log(max(6,3,4))
 }
